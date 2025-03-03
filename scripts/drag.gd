4df4b10a61989316e89eb62765229fd9763ff0c7
BlazingNeutron <steven.armstrong+github@gmail.com>
2025-03-03 15:19:34 -0500

    Upgraded path follower for curves

--- scripts/drag.gd
+++ scripts/drag.gd
@@ -4,26 +4,41 @@ extends Node2D
 
 var path_index = 0
 var path : Path2D = null
+var pressed = false
 var dragging = false
-var touching = false
 var path_follow : PathFollow2D = null
 var sprite : Sprite2D = null
+var curve_index = 1
 
 func _ready() -> void:
 	path_index = -1
 	next_path()
 
 func _physics_process(delta: float) -> void:
-	if dragging:
+	if get_global_mouse_position().distance_to(sprite.global_position) > 50:
+		dragging = false
+	var next_path_point = path.curve.get_baked_points()[curve_index]
+	var pull_direction = sprite.global_position.direction_to(get_global_mouse_position())
+	var goal_direction = sprite.global_position.direction_to(next_path_point)
+	if curve_index >= 1:
+		var not_the_goal_direction = sprite.global_position.direction_to(path.curve.get_baked_points()[curve_index - 1])
+		if pull_direction.dot(not_the_goal_direction) > 0:
+			dragging = false
+	if pull_direction.dot(goal_direction) < 0:
+		if path.curve.get_baked_points().size() > curve_index + 1:
+			var next_goal_direction = sprite.global_position.direction_to(path.curve.get_baked_points()[curve_index + 1])
+			if pull_direction.dot(next_goal_direction) > 0:
+				curve_index += 1
+	if pressed and dragging:
 		path_follow.progress += calc_offset(sprite, path)
 	if path_follow.progress_ratio >= 1.0:
 		path_follow.hide()
 		dragging = false
-		touching = false
 		next_path()
 
 func next_path() -> void:
 	path_index += 1
+	curve_index = 1
 	if path_index >= letter_paths.get_child_count():
 		return
 	path = letter_paths.get_children()[path_index]
@@ -39,14 +54,10 @@ func calc_offset(sprite : Sprite2D, path : Path2D) -> float:
 	return distance
 
 func _input(event: InputEvent) -> void:
-	if get_global_mouse_position().distance_to(sprite.global_position) < 60:
-		touching = true
 	if event.is_action_pressed("touch"):
-		dragging = true
+		pressed = true
 	elif event.is_action_released("touch"):
+		pressed = false
 		dragging = false
-	if event is InputEventMouseMotion and dragging:
-		var pull_direction = sprite.global_position.direction_to(get_global_mouse_position())
-		var goal_direction = sprite.global_position.direction_to(path.curve.get_point_position(1))
-		if pull_direction.dot(goal_direction) > 0:
-			dragging = true
+	if event is InputEventMouseMotion and pressed:
+		dragging = true
